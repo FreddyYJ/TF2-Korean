fe32dd20a004d4b60e64801318b12a6b7f64ca36
김영재 <kyj141100@naver.com>
2021-03-14 12:47:42 +0900

    Init project

---
+++ ko/info.lua
@@ -0,0 +1,12 @@
+function data()
+return {
+	name = "Korean",
+	locale = "ko_KR",
+	fontMap = {
+		regular = "Korean/NanumGothic.ttf",
+		bold = "Korean/NanumGothicBold.ttf",
+		monoRegular = "Korean/NanumGothicCoding.ttf",
+		monoBold = "Korean/NanumGothicCoding-Bold.ttf",
+	},
+}
+end
